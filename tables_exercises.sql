d2fd30a382c8c12cff13a877cf2cb9eb79948f76
Alicia Gonzalez <alicia.j.gonzalez01@gmail.com>
2020-12-16 18:58:34 -0600

    New table exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+SHOW tables;
+
+
+describe employees;
+
+describe employees_with_departments;
+describe salaries;
+describe titles;
+describe departments;
+describe dept_emp;
+describe dept_manager;
+
+SHOW CREATE TABLE dept_manager;
