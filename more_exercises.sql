a30771715b9b0cf9f9b12f58e647168fd1fd042d
Alicia Gonzalez <alicia.j.gonzalez01@gmail.com>
2021-04-07 18:46:54 -0500

    working on joins

--- more_exercises.sql
+++ more_exercises.sql
@@ -421,78 +421,191 @@ LIMIT 5;
 
 #1. SELECT statements
 #1a. Select all columns from the actor table.
+SELECT * 
+FROM actor;
 
 #1b. Select only the last_name column from the actor table.
+SELECT last_name 
+FROM actor;  #200 rows
 
-#1c. Select only the following columns from the film table.
-
+#1c. Select only the following columns from the film table. #what?
+SELECT *
+FROM film;
 
 #2. DISTINCT operator
 #2a. Select all distinct (different) last names from the actor table.
+SELECT DISTINCT last_name
+FROM actor; #121 rows
 
 #2b. Select all distinct (different) postal codes from the address table.
+SELECT DISTINCT postal_code
+FROM address;
 
 #2c. Select all distinct (different) ratings from the film table.
-
+SELECT DISTINCT rating
+FROM film;
 
 #3. WHERE clause
 #3a. Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+SELECT title, description, rating, length
+FROM film
+WHERE length >= 180;
 
 #3b. Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date >= '05/27/2005'; 
 
 #3c. Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date = '05/27/2005'; 
 
 #3d. Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N.
+SELECT *
+FROM customer
+WHERE last_name LIKE 'S%' AND first_name LIKE '%N';
 
 #3e. Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
+SELECT * 
+FROM customer
+WHERE active = 0 OR last_name LIKE 'M%';
 
 #3f. Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T.
+SELECT * 
+FROM category
+WHERE category_id > 4 AND name LIKE 'C%' OR name LIKE 'S%' OR name LIKE 'T%';
 
 #3g. Select all columns minus the password column from the staff table for rows that contain a password.
+SELECT staff_id,
+	first_name,
+	last_name,
+	address_id,
+	picture,
+	email,
+	store_id,
+	active,
+	username,
+	last_update
+FROM staff
+WHERE PASSWORD IS NOT NULL; 
 
 #3h. Select all columns minus the password column from the staff table for rows that do not contain a password.
+SELECT staff_id,
+	first_name,
+	last_name,
+	address_id,
+	picture,
+	email,
+	store_id,
+	active,
+	username,
+	last_update
+FROM staff
+WHERE PASSWORD IS NULL;
 
 
 #4.IN operator
 #4a. Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
+SELECT phone, district
+FROM address
+WHERE district IN ('California', 'England', 'Taipei', 'West');
 
 #4b. Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE DATE(payment_date) IN (DATE("2005-05-25"),DATE("2005-05-27"),DATE("2005-05-29"));
+
+#OR 
+
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE YEAR(payment_date) IN (2005) AND MONTH(payment_date) IN (05) AND DAY(payment_date) IN (25, 27, 29);
 
 #4c. Select all columns from the film table for films rated G, PG-13 or NC-17.
-
+SELECT * 
+FROM film 
+WHERE rating IN ('G', 'PG-13', 'NC-17');
 
 #5. BETWEEN operator
 #5a. Select all columns from the payment table for payments made between midnight 05/25/2005 and 1 second before midnight 05/26/2005.
+SELECT * FROM payment 
+WHERE payment_date BETWEEN "2005-05-25 00:00:00" AND "2005-05-27 23:59:59";
 
 #5b. Select the following columns from the film table for films where the length of the description is between 100 and 120.
 #Hint: total_rental_cost = rental_duration * rental_rate
-
+SELECT title,
+	rental_duration * rental_rate AS total_rental_cost,
+	length(description)
+FROM film
+WHERE length(description) BETWEEN 100 AND 120;
 
 #6. LIKE operator
 #6a. Select the following columns from the film table for rows where the description begins with "A Thoughtful".
+SELECT * 
+FROM film 
+WHERE description LIKE 'A Thoughtful%';
 
 #6b. Select the following columns from the film table for rows where the description ends with the word "Boat".
+SELECT *
+FROM film
+WHERE description LIKE '%Boat';
 
 #6c. Select the following columns from the film table where the description contains the word "Database" and the length of the film is greater than 3 hours.
-
+SELECT * 
+FROM film 
+WHERE description LIKE '%Database%' AND length > 180;
 
 #7. LIMIT Operator
 #7a. Select all columns from the payment table and only include the first 20 rows.
+SELECT *
+FROM payment
+LIMIT 20;
 
 #7b. Select the payment date and amount columns from the payment table for rows where the payment amount is greater than 5, and only select rows whose zero-based index in the result set is between 1000-2000.
+#not sure which is the correct one
+SELECT payment_id, payment_date, amount
+FROM payment
+WHERE amount > 5
+LIMIT 1000
+OFFSET 1001;
+
+SELECT payment_id, amount
+FROM payment
+WHERE amount > 5
+LIMIT 240 OFFSET 246
+
 
 #7c. Select all columns from the customer table, limiting results to those where the zero-based index is between 101-200.
-
+SELECT * 
+FROM customer
+LIMIT 100 OFFSET 100;
 
 #8. ORDER BY statement
 #8a. Select all columns from the film table and order rows by the length field in ascending order.
+SELECT * 
+FROM film 
+ORDER BY length;
 
 #8b. Select all distinct ratings from the film table ordered by rating in descending order.
+SELECT DISTINCT rating
+FROM film
+ORDER BY rating DESC;
 
 #8c. Select the payment date and amount columns from the payment table for the first 20 payments ordered by payment amount in descending order.
+SELECT payment_date, amount
+FROM payment
+ORDER BY amount DESC
+LIMIT 20;
 
 #8d. Select the title, description, special features, length, and rental duration columns from the film table for the first 10 films with behind the scenes footage under 2 hours in length and a rental duration between 5 and 7 days, ordered by length in descending order.
-
+SELECT title, description, special_features, length, rental_duration
+FROM film
+WHERE special_features LIKE '%Behind the Scenes%' 
+	AND length < 120 
+	AND rental_duration BETWEEN 5 AND 8
+ORDER BY length DESC
+LIMIT 10;
 
 #9. JOINs
 #9a. Select customer first_name/last_name and actor first_name/last_name columns from performing a left join between the customer and actor column on the last_name column in each table. (i.e. customer.last_name = actor.last_name)
